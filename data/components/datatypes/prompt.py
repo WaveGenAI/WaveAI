82dffea2f905494a6fc6f4788796d847f24c33b1
Jourdelune <jourdelune863@gmail.com>
2024-07-08 11:54:39 +0200

    [update] remove data/components 	deleted:    data/components/config.py 	deleted:    data/components/datatypes/lyrics.py 	deleted:    data/components/datatypes/music.py 	deleted:    data/components/datatypes/prompt.py 	deleted:    data/components/prompt 	deleted:    data/components/prompt_gen.py

--- data/components/datatypes/prompt.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Prompt:
-    """Class that represent the prompt that will be passed as an input for the music generation model.
-    """
-
-    content: str
