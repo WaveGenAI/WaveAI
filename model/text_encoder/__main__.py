4b4ebcd845266be14b8dee09f28e69507ca31385
Jourdelune <jourdelune863@gmail.com>
2024-07-09 22:00:23 +0200

    [update] add t5 encoder

---
+++ model/text_encoder/__main__.py
@@ -0,0 +1,4 @@
+from encoder import T5EncoderBaseModel
+
+test = T5EncoderBaseModel()
+print(test("hello world"))
---
+++ model/text_encoder/encoder.py
@@ -0,0 +1,34 @@
+""" 
+Encoder Model for Text Data
+"""
+
+import torch
+import torch.nn as nn
+from transformers import T5EncoderModel, AutoTokenizer
+
+
+class T5EncoderBaseModel(nn.Module):
+    """
+    T5 Encoder Model, which is used to encode the input text into a latent space.
+    """
+
+    def __init__(self, name: str = "t5-base"):
+        super().__init__()
+
+        self.encoder = T5EncoderModel.from_pretrained(name)
+        self.tokenizer = AutoTokenizer.from_pretrained(name)
+
+    def forward(self, inputs: str) -> torch.Tensor:
+        """Forward pass of the model
+
+        Args:
+            inputs (str): the input text
+
+        Returns:
+            torch.Tensor: the latent space of the input text
+        """
+
+        input_ids = self.tokenizer(inputs, return_tensors="pt").input_ids
+        latent_space = self.encoder(input_ids)
+
+        return latent_space
