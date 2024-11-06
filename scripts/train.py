fix gradient exploding because if mixed precision

scripts/train.py
@@ -38,6 +38,10 @@ lr_monitor = LearningRateMonitor(logging_interval="step")
 audio_processor = AudioProcessor(config)
 model = Trainer(config, audio_processor)
 
+torch.autograd.set_detect_anomaly(True)
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+
 for p in model.parameters():
     if p.dim() > 1:
         torch.nn.init.normal_(p, 0, 0.02)
@@ -107,7 +111,6 @@ if __name__ == "__main__":
         logger=wandb_logger,
         log_every_n_steps=1,
         default_root_dir=args.save_path,
-        precision=16,
         profiler="simple",
         **kwargs,
     )

waveai/audio_processor.py
@@ -77,9 +77,19 @@ class AudioProcessor:
         padded_codebooks = torch.nn.utils.rnn.pad_sequence(
             codebooks, padding_value=pad_token
         )
+
         # permute to b, k, seq_len
         padded_codebooks = padded_codebooks.permute(1, 2, 0)
-        return padded_codebooks
+
+        pad_full_length = torch.zeros(
+            padded_codebooks.size(0),
+            padded_codebooks.size(1),
+            self.config.model.max_seq_length,
+        ).fill_(pad_token)
+
+        pad_full_length[:, :, : padded_codebooks.size(2)] = padded_codebooks
+
+        return pad_full_length.long()
 
     @torch.no_grad()
     def collate_fn(self, rows: list) -> tuple:
@@ -99,7 +109,7 @@ class AudioProcessor:
         # get the size of the inputs (seq_length)
         inputs_size = torch.tensor([input.size(-1) for input in inputs])
         # create the padding mask
-        padding_mask = make_pad_mask(inputs_size)
+        padding_mask = make_pad_mask(inputs_size, self.config.model.max_seq_length)
 
         # Pad the codec tensors and stack them
         inputs = self._pad_codebooks(inputs, pad_token=self.config.model.pad_token_id)
@@ -107,8 +117,18 @@ class AudioProcessor:
         # encode the prompt
         prompts_embeds, prompts_masks = self.encode_prompt(prompt)
 
+        # audioMAE embeds for cosine similarity
+        labels_mae = [
+            torch.Tensor(row[self.config.data.audio_embed_column]) for row in rows
+        ]
+
+        labels_mae = [label.view(label.shape[0], -1) for label in labels_mae]
+
+        # stack the audio embeds
+        labels_mae = torch.stack(labels_mae)
+
         # TODO: Maybe remove prompt from the return because it's a str and not a tensor (but currently used for logging)
-        return inputs, padding_mask, prompts_embeds, prompts_masks, prompt
+        return inputs, padding_mask, prompts_embeds, prompts_masks, labels_mae, prompt
 
     @torch.no_grad()
     def decode_audio(self, codec: torch.Tensor) -> AudioSignal:

waveai/model.py
@@ -1,216 +1,31 @@
-import math
-
 import torch
-import torch.nn.functional as F
-from huggingface_hub import hf_hub_download
-from rotary_embedding_torch import RotaryEmbedding
 from torch import nn
-from torch.nn.attention import SDPBackend, sdpa_kernel
 from x_transformers import Decoder, MultiInputTransformerWrapper
 
 
-class MultiheadAttention(nn.Module):
-    def __init__(
-        self,
-        embed_dim: int,
-        num_heads: int,
-        causal: bool,
-        cross_attention: bool,
-        rotary_emb: bool,
-    ):
+class REPA(nn.Module):
+    """Project the input sequence to a fixed size representation
+    that can be used to compute the cosine similarity with an embedding
+    https://arxiv.org/pdf/2410.06940
+    """
+
+    def __init__(self, hidden_size=512, projector_dim=2048, z_dim=768):
         super().__init__()
-
-        self.embed_dim = embed_dim
-        self.num_heads = num_heads
-        self.causal = causal
-        self.cross_attention = cross_attention
-        self.decoder = True
-
-        self.in_proj_weight = nn.Parameter(torch.empty((embed_dim * 3, embed_dim)))
-        nn.init.kaiming_uniform_(self.in_proj_weight, a=math.sqrt(5))
-
-        self.out_proj = nn.Linear(embed_dim, embed_dim, bias=False)
-
-        self.key_cache = None
-        self.value_cache = None
-
-        if rotary_emb:
-            rotary_dim = (
-                embed_dim // num_heads
-            ) // 2  # rotary embedding dim = dim of each head / 2
-            self.rotary_emb = RotaryEmbedding(dim=rotary_dim)
-        else:
-            self.rotary_emb = None
-
-    def forward(self, query, key, value, key_padding_mask=None):
-        if not self.cross_attention:
-            key = query
-            value = query
-
-        q = F.linear(query, self.in_proj_weight[: self.embed_dim])
-        k = F.linear(key, self.in_proj_weight[self.embed_dim : 2 * self.embed_dim])
-        v = F.linear(value, self.in_proj_weight[2 * self.embed_dim :])
-
-        q, k, v = [
-            x.reshape(
-                x.shape[0], x.shape[1], self.num_heads, x.shape[2] // self.num_heads
-            )
-            for x in [q, k, v]
-        ]
-
-        q = q.transpose(1, 2)
-        k = k.transpose(1, 2)
-        v = v.transpose(1, 2)
-
-        B, _, T, _ = q.shape
-
-        if self.rotary_emb is not None:
-            # apply rotary embedding to queries and keys (if not cross-attention)
-            q = self.rotary_emb.rotate_queries_or_keys(q)
-
-            if not self.cross_attention:
-                k = self.rotary_emb.rotate_queries_or_keys(k)
-
-        x = flash_attention(q, k, v, causal=self.causal, padding_mask=key_padding_mask)
-        x = x.view(B, T, self.embed_dim)
-        x = self.out_proj(x)
-        return x, None
-
-
-def get_causal_mask(size: int):
-    queries_pos = torch.arange(size).view(-1, 1)
-    keys_pos = torch.arange(size).view(1, -1)
-    return torch.where(
-        (queries_pos - keys_pos) >= 0, torch.zeros([]), torch.full([], float("-inf"))
-    )
-
-
-def flash_attention(q, k, v, causal=True, padding_mask=None):
-    B, h, T, d = q.shape
-
-    # raise error if causal is true and padding_mask is not None
-    assert not (
-        causal and padding_mask is not None
-    ), "Causal and padding_mask not supported together"
-
-    if padding_mask is not None:
-        # torch.Size([1, 1, 1, T])
-        padding_mask = padding_mask.unsqueeze(1).unsqueeze(2)
-        padding_mask = padding_mask.to(torch.bool)
-
-    # apply flash attention
-    with sdpa_kernel(
-        [SDPBackend.FLASH_ATTENTION, SDPBackend.MATH, SDPBackend.EFFICIENT_ATTENTION]
-    ):
-        x = F.scaled_dot_product_attention(q, k, v, is_causal=causal)
-
-    x = x.transpose(1, 2).reshape(B, T, d * h)
-    return x
-
-
-def inefficient_attention(q, k, v, causal=True):
-    B, h, T, d = q.shape
-    embed_dim = d * h
-
-    attention = q.matmul(k.transpose(2, 3)) / math.sqrt(embed_dim // h)
-
-    if causal:
-        attn_mask = (
-            get_causal_mask(q.shape[2])
-            .unsqueeze(0)
-            .unsqueeze(0)
-            .to(q.device)
-            .to(q.dtype)
-        )
-        attention += attn_mask
-    activation = torch.softmax(attention, dim=-1)
-    x = (
-        v.unsqueeze(2).repeat([1, 1, T, 1, 1])
-        * activation.unsqueeze(-1).repeat([1, 1, 1, 1, 64])
-    ).sum(dim=3)
-    x = x.transpose(1, 2).reshape(B, T, d * h)
-    return x
-
-
-def create_sin_embedding(positions: torch.Tensor, dim: int) -> torch.Tensor:
-    assert dim % 2 == 0
-    half_dim = dim // 2
-    positions = positions
-    adim = torch.arange(half_dim, device=positions.device, dtype=positions.dtype).view(
-        1, 1, -1
-    )
-    max_period_tensor = torch.full(
-        [], 10_000, device=positions.device
-    )  # avoid sync point
-    phase = positions / (max_period_tensor ** (adim / (half_dim - 1)))
-    return torch.cat([torch.cos(phase), torch.sin(phase)], dim=-1)
-
-
-class TransformerLayer(nn.Module):
-    def __init__(self, dim=1024, num_heads=16, ff_dim=4096, rotary_emb=False):
-        super().__init__()
-        self.norm1 = nn.LayerNorm(dim)
-        self.self_attn = MultiheadAttention(
-            embed_dim=dim,
-            causal=True,  # big difference!
-            cross_attention=False,
-            num_heads=num_heads,
-            rotary_emb=rotary_emb,
-        )
-        self.norm_cross = nn.LayerNorm(dim)
-        self.cross_attention = MultiheadAttention(
-            embed_dim=dim,
-            causal=False,
-            cross_attention=True,
-            num_heads=num_heads,
-            rotary_emb=rotary_emb,
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_size, projector_dim),
+            nn.SiLU(),
+            nn.Linear(projector_dim, projector_dim),
+            nn.SiLU(),
+            nn.Linear(projector_dim, z_dim),
         )
 
-        self.norm2 = nn.LayerNorm(dim)
-        self.linear1 = nn.Linear(dim, ff_dim, bias=False)
-        self.linear2 = nn.Linear(ff_dim, dim, bias=False)
+    def forward(self, x):
+        B, T, D = x.shape
 
-    def forward(self, x, memory, memory_key_padding_mask):
-        x_ = self.norm1(x)
-        x = x + self.self_attn(x_, x_, x_)[0]
+        # apply the mlp
+        x = self.mlp(x.reshape(-1, D))
+        x = x.reshape(B, T, -1)
 
-        x_ = self.norm_cross(x)
-        x = x + self.cross_attention(x_, memory, memory, memory_key_padding_mask)[0]
-
-        x_ = self.norm2(x)
-        x = x + self.linear2(F.gelu(self.linear1(x_)))
-        return x
-
-
-class Transformer(nn.Module):
-    def __init__(
-        self,
-        dim: int = 1024,
-        depth: int = 24,
-        ff_dim: int = 4096,
-        heads: int = 16,
-        rotary_emb: bool = False,
-    ):
-        super().__init__()
-
-        self.layers = nn.ModuleList(
-            [TransformerLayer(dim, heads, ff_dim, rotary_emb) for _ in range(depth)]
-        )
-        self._rotary_emb = rotary_emb
-
-    def pos_embed(self, x):
-        B, T, dim = x.shape
-        positions = (
-            torch.arange(T, device=x.device).view(1, -1, 1).to(x.dtype).to(x.device)
-        )
-        x = x + create_sin_embedding(positions, dim)
-        return x
-
-    def forward(self, x, memory, memory_key_padding_mask):
-        if not self._rotary_emb:
-            x = self.pos_embed(x)
-        for layer in self.layers:
-            x = layer(x, memory, memory_key_padding_mask)
         return x
 
 
@@ -227,6 +42,7 @@ class WaveAI(nn.Module):
         num_heads: int,
         memory_dim: int,
         rotary_emb: bool = False,
+        repa_pos: int = 3,
     ):
         super().__init__()
         self.num_codebooks = codebook_count
@@ -261,6 +77,9 @@ class WaveAI(nn.Module):
         else:
             self.memory_proj = nn.Identity()
 
+        self.repa = REPA(hidden_size=dim)
+        self._repa_pos = repa_pos
+
     def forward(
         self,
         x: torch.Tensor,
@@ -290,11 +109,12 @@ class WaveAI(nn.Module):
         for k in range(self.num_codebooks):
             x_bis[f"codebook {k}"] = x[:, k, :]
 
-        out = self.transformer(
+        out, intermediates = self.transformer(
             x_bis,
             mask=x_padding_mask,
             context=memory,
             context_mask=memory_key_padding_mask,
+            return_intermediates=True,
         )  # returns a dict with keys "codebook 0", "codebook 1", ... and values the probabilities for each token
 
         # stack the codebooks predictions to get the logits of shape (batch_size, num_codebooks, length, vocab_size)
@@ -302,14 +122,7 @@ class WaveAI(nn.Module):
             [out[f"codebook {k}"] for k in range(self.num_codebooks)], dim=1
         )
 
-        return stacked_out
+        # get the intermediate representation
+        intermediate_repr = self.repa(intermediates.hiddens[self._repa_pos])
 
-    def load_pretrained(self, device, model="facebook/musicgen-small"):
-        """Load musicgen to test the model"""
-
-        path = hf_hub_download(repo_id=model, filename="state_dict.bin", cache_dir=None)
-        _values = torch.load(path, map_location=device)
-        state_dict = {
-            k: v for k, v in _values["best_state"].items() if k in self.state_dict()
-        }
-        self.load_state_dict(state_dict)
+        return stacked_out, intermediate_repr